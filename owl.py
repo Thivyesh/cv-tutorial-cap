d6f6b81100ac6b4a3c32a8b051fda514fd991eb3
Thivyesh Ahilathasan <a.thivy@gmail.com>
2024-04-29 18:45:11 +0200

    feature: hf models work

---
+++ owl.py
@@ -0,0 +1,54 @@
+import cv2
+from transformers import pipeline
+from PIL import ImageDraw
+from PIL import Image
+import numpy as np
+
+#Initialize detection pipeline
+checkpoint = "google/owlv2-base-patch16-ensemble"
+detector = pipeline(model=checkpoint, task="zero-shot-object-detection")
+
+#Initialize webcam capture
+cap = cv2.VideoCapture(0)
+
+if not cap.isOpened():
+    print("Cannot open camera")
+    exit()
+while True:
+    ret, frame =  cap.read()
+
+    if not ret:
+        print("Can't receive frame (stream end?), Exiting ...")
+        break
+
+    # #Convert image to a likeable format
+    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    frame = Image.fromarray(frame)
+    
+    #predict
+    predictions = detector(
+        frame,
+        candidate_labels=["human face"]
+    )
+    print(predictions)
+
+
+    #annotate frame
+    draw = ImageDraw.Draw(frame)
+    for prediction in predictions:
+        box = prediction["box"]
+        label = prediction["label"]
+        score = prediction["score"]
+
+        xmin, ymin, xmax, ymax = box.values()
+        draw.rectangle((xmin, ymin, xmax, ymax), outline="red", width=1)
+        draw.text((xmin, ymin), f"{label}: {round(score, 2)}", fill="white")
+    frame = np.array(frame)
+    frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
+
+    cv2.imshow("frame", frame)
+    if cv2.waitKey(1) == ord("q"):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
